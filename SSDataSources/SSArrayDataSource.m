db3ceb25ad0f5ef6192a70edfbfe8599c4394bdd
Andrew Sardone <andrew@andrewsardone.com>
2014-04-06 16:03:44 -0400

    Change `target` ownership to strong, fix -test_no_exception_if_target_goes_away
    I'd still [cast a vote][c] that this will limit the functionality of
    SSArrayDataSource's KVOing abilities, but I think it's a worthy
    trade-off to prevent a crash.
    
    [c]: https://github.com/splinesoft/SSDataSources/pull/29#issuecomment-39678236

--- SSDataSources/SSArrayDataSource.m
+++ SSDataSources/SSArrayDataSource.m
@@ -22,7 +22,7 @@ static void *SSArrayKeyPathDataSourceContext = &SSArrayKeyPathDataSourceContext;
  * The object that the receiver is observing at the given key path when initialized
  * via -initwithitems:.
  */
-@property (nonatomic, unsafe_unretained) id target;
+@property (nonatomic, strong) id target;
 
 /**
  * The key path for an NSArray off of target the receiver is initialized via
